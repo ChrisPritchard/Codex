2fa776e6ba94ee3936e377c96f4b86991fbad643
Christopher <chrispritchard.nz@gmail.com>
2019-10-04 19:25:51 +1300

    work on bindable rich text box

---
+++ src/Codex.Views/BindableRichTextBox.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace Codex.Views
+{
+    public partial class BindableRichTextBox : UserControl
+    {
+        public BindableRichTextBox()
+        {
+            InitializeComponent();
+            CoreRichTextBox.Focus();
+        }
+
+        public FlowDocument Document
+        {
+            get { return (FlowDocument)GetValue(DocumentProperty); }
+            set { SetValue(DocumentProperty, value); }
+        }
+
+        public static readonly DependencyProperty DocumentProperty =
+            DependencyProperty.Register("Document", 
+                typeof(FlowDocument), typeof(BindableRichTextBox), new PropertyMetadata(OnDocumentChanged));
+
+        private static void OnDocumentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = (BindableRichTextBox)d;
+            if (e.NewValue == null)
+                control.CoreRichTextBox.Document = new FlowDocument(); // Document is not amused by null :)
+            else
+                control.CoreRichTextBox.Document = (FlowDocument)e.NewValue;
+        }
+    }
+}
