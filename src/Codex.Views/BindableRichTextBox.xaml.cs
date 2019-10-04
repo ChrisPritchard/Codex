e72ddef5d6d40071a6ae64812554abe771e37dc2
Christopher <chrispritchard.nz@gmail.com>
2019-10-05 09:50:13 +1300

    finally got binding working...so far

--- src/Codex.Views/BindableRichTextBox.xaml.cs
+++ src/Codex.Views/BindableRichTextBox.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
 using System.IO;
 using System.Text;
 using System.Windows;
@@ -8,7 +7,7 @@ using System.Windows.Documents;
 
 namespace Codex.Views
 {
-    public partial class BindableRichTextBox : UserControl, INotifyPropertyChanged
+    public partial class BindableRichTextBox : UserControl
     {
         public BindableRichTextBox()
         {
@@ -16,41 +15,39 @@ namespace Codex.Views
             CoreRichTextBox.Focus();
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        private static readonly DependencyProperty WordCountProperty =
+            DependencyProperty.Register(nameof(WordCount), typeof(int), typeof(BindableRichTextBox));
+        private static readonly DependencyProperty IsDirtyProperty =
+            DependencyProperty.Register(nameof(IsDirty), typeof(bool), typeof(BindableRichTextBox));
+        private static readonly DependencyProperty XamlContentProperty =
+            DependencyProperty.Register(nameof(XamlContent), typeof(string), typeof(BindableRichTextBox));
+
+        public int WordCount { get => (int)GetValue(WordCountProperty); private set => SetValue(WordCountProperty, value); }
+        public bool IsDirty { get => (bool)GetValue(IsDirtyProperty); private set => SetValue(IsDirtyProperty, value); }
+        public string XamlContent { get => (string)GetValue(XamlContentProperty); set => SetValue(XamlContentProperty, value); }
 
         private TextRange CurrentRange => new TextRange(CoreRichTextBox.Document.ContentStart, CoreRichTextBox.Document.ContentEnd);
 
-        public string XamlContent
+        private string Xaml
         {
-            get 
+            get
             {
                 using var stream = new MemoryStream();
                 CurrentRange.Save(stream, DataFormats.Xaml);
                 return Encoding.UTF8.GetString(stream.ToArray());
             }
-            set 
+            set
             {
                 using var stream = new MemoryStream(Encoding.UTF8.GetBytes(value));
                 CurrentRange.Load(stream, DataFormats.Xaml);
-                TextDirty = false;
             }
         }
-
-        public int WordCount { get; private set; }
-
-        public bool TextDirty { get; private set; }
 
         private void TextChanged(object sender, TextChangedEventArgs e)
         {
             WordCount = CurrentRange.Text.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
-            TextDirty = true;
-
-            if (PropertyChanged == null)
-                return;
-
-            PropertyChanged(this, new PropertyChangedEventArgs(nameof(XamlContent)));
-            PropertyChanged(this, new PropertyChangedEventArgs(nameof(WordCount)));
-            PropertyChanged(this, new PropertyChangedEventArgs(nameof(TextDirty)));
+            IsDirty = true;
+            XamlContent = Xaml;
         }
     }
 }
