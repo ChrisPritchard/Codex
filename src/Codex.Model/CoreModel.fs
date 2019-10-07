db8caeacc6788a152a21a9c04ba5e8cfb45fb375
Christopher <chrispritchard.nz@gmail.com>
2019-10-08 07:05:20 +1300

    fixed up reference syntax

--- src/Codex.Model/CoreModel.fs
+++ src/Codex.Model/CoreModel.fs
@@ -1,22 +1,24 @@
-﻿module Core.Models.CoreModel
+﻿namespace Codex.Model
 
-type Novel = {
-    title: string option
-    synopsis: string
-    acts: Act list
-}
-and Act = {
-    title: string option
-    synopsis: string
-    chapters: Chapter list
-}
-and Chapter = {
-    title: string option
-    synopsis: string
-    scenes: Scene list
-}
-and Scene = {
-    outline: string
-    content: string
-    wordCount: int
-}
+module CoreModel =
+
+    type Novel = {
+        title: string option
+        synopsis: string
+        acts: Act list
+    }
+    and Act = {
+        title: string option
+        synopsis: string
+        chapters: Chapter list
+    }
+    and Chapter = {
+        title: string option
+        synopsis: string
+        scenes: Scene list
+    }
+    and Scene = {
+        outline: string
+        content: string
+        wordCount: int
+    }
