53e565b6f6bf72f0d879f0c1364c6fd1607ccb11
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-10-08 14:03:26 +1300

    expanded test model

--- src/Codex.Model/MainWindow.fs
+++ src/Codex.Model/MainWindow.fs
@@ -24,6 +24,82 @@ let testNovel: TableOfContents.Novel = {
                         title = "Chapter 1"
                         scenes = [
                             { wordCount = 20 }
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                        ]
+                    }
+                    {
+                        title = "Chapter 2"
+                        scenes = [
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                        ]
+                    }
+                    {
+                        title = "Chapter 3"
+                        scenes = [
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                        ]
+                    }
+                ]
+            }
+            {
+                title = "Act 2"
+                chapters = [
+                    {
+                        title = "Chapter 4"
+                        scenes = [
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                        ]
+                    }
+                    {
+                        title = "Chapter 5"
+                        scenes = [
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                        ]
+                    }
+                    {
+                        title = "Chapter 6"
+                        scenes = [
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                        ]
+                    }
+                ]
+            }
+            {
+                title = "Act 3"
+                chapters = [
+                    {
+                        title = "Chapter 7"
+                        scenes = [
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                        ]
+                    }
+                    {
+                        title = "Chapter 8"
+                        scenes = [
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                        ]
+                    }
+                    {
+                        title = "Chapter 9"
+                        scenes = [
+                            { wordCount = 20 }
+                            { wordCount = 20 }
+                            { wordCount = 20 }
                         ]
                     }
                 ]
