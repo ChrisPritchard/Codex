995b4427d03144fab14790d0276cf04c78c70f23
Christopher <chrispritchard.nz@gmail.com>
2019-10-10 21:25:39 +1300

    concept data template selector

---
+++ src/Codex.Model/Test.fs
@@ -0,0 +1,18 @@
+﻿namespace Codex.Model
+
+module Core =
+
+    type Part = 
+        | Grouping of Grouping
+        | Content of Content
+        with 
+            member p.AsGrouping = match p with Grouping g -> (true, g) | _ -> (false, Unchecked.defaultof<_>)
+            member p.AsContent = match p with Content c -> (true, c) | _ -> (false, Unchecked.defaultof<_>)
+    and Grouping = {
+        title: string
+        parts: Part list
+        }
+    and Content = {
+        wordCount: int
+        xamlContent: string
+        }
